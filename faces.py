fc872938119b5ae2aa9c1f71f1c0fde14ba93cee
rishabhtyagi2306 <rishabhtyagi.2306@gmail.com>
2020-08-26 21:46:01 +0530

    Created GUI

--- faces.py
+++ faces.py
@@ -1,30 +1,39 @@
 import numpy as np
+from tkinter import *
 import cv2
 
-face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
-cap = cv2.VideoCapture(0)
-
-while True:
-    ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
-    for (x, y, w, h) in faces:
-        print(x, y, w, h)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = frame[y:y+h, x:x+w]
-        img_item = "my_image.png"
-        cv2.imwrite(img_item, roi_gray)
-
-        color = (255, 0, 0)
-        stroke = 2
-        end_cord_y = y+h
-        end_cord_x = x+w
-        cv2.rectangle(frame, (x, y), (end_cord_x, end_cord_y), color, stroke)
-
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+root = Tk()
+root.geometry('500x600')
+heading = Label(root, text="Welcome! to the super secure face recognition system",
+                font=('montserrat', 12, "bold"), fg="black").pack()
 
 
-cap.release()
-cv2.destroyAllWindows()
+def on_click():
+    my_label = Label(root, text="Look into the camera")
+    my_label.pack()
+    face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+    cap = cv2.VideoCapture(0)
+
+    if cap.isOpened():
+        ret, frame = cap.read()
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+        if faces is ():
+            mylabel = Label(root, text="Sorry could not find your face try again")
+            mylabel.pack()
+        else:
+            for (x, y, w, h) in faces:
+                print(x, y, w, h)
+                roi_gray = gray[y:y + h, x:x + w]
+                roi_color = frame[y:y + h, x:x + w]
+                img_item = "my_image.png"
+                cv2.imwrite(img_item, roi_color)
+
+    cap.release()
+    # cv2.destroyAllWindows()
+
+
+my_button = Button(root, text="click here to open VS Code", command=on_click)
+my_button.pack()
+
+root.mainloop()
