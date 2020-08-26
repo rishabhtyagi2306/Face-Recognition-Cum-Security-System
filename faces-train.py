63387ed84b75c515d392beb8d9d154613f3fa822
rishabhtyagi2306 <rishabhtyagi.2306@gmail.com>
2020-08-26 16:51:34 +0530

    Deleted Extra images

---
+++ faces-train.py
@@ -0,0 +1,10 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+image_dir = os.path.join(BASE_DIR, "images")
+
+for root, dirs, files in os.walk(image_dir):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            print(path)
